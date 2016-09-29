bb5e034dd472c793f9024864be91a91e7f320c91
yury-g <yury.gitman@gmail.com>
2016-09-29 10:46:59 -0400

    Simple Text Input and Speak, with Rate Adjustment

--- SimpleSpeakApp/ViewController.swift
+++ SimpleSpeakApp/ViewController.swift
@@ -7,11 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+//  ViewController Class-Wide Variables
+let mySpeechSynth = AVSpeechSynthesizer()
+var floatRate: Float = 0.5
+var stringRate = "0.5"
+
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var rateSpeedLabel: UILabel!
+    
+    
+    @IBAction func speakPressed(_ sender: UIButton) {
+        
+        let usersWord = myTextField.text
+        
+        speakThisPhrase(passedString: usersWord!)
+        
+    }
+    @IBAction func sliderMoved(_ sender: UISlider) {
+            floatRate = sender.value
+            stringRate = String(floatRate)
+            rateSpeedLabel.text = stringRate
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myTextField.text = "type a word here and press 'Speak'"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +49,19 @@ class ViewController: UIViewController {
     }
 
 
+    
+    // from:
+    // Pass a String, This Function Speaks it.
+    // Change "rate" and "voice" to hear different voices. 
+    func speakThisPhrase(passedString: String){
+        
+        let myUtterance = AVSpeechUtterance(string: passedString)
+        myUtterance.rate = floatRate
+        myUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        mySpeechSynth.speak(myUtterance)
+        
+        
+    }
+    
 }
 
